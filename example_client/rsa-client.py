9f60f385b341427fe07bffdf893a7d49007e6dff
roctbb <roctbb@gmail.com>
2021-10-28 00:08:22 +0300

    mvp server

--- example_client/rsa-client.py
+++ example_client/rsa-client.py
@@ -1,13 +1,16 @@
 import socket
 
-from client.config import PUBLIC_KEY, PRIVATE_KEY
+from example_client.config import PUBLIC_KEY, PRIVATE_KEY
 from common import netlib, rsalib
 from common import byteslib
 import json
 
+# 0. соединение с сервером
 sock = socket.socket()
 sock.connect(('127.0.0.1', 9090))
 
+# 1. обмен ключами
+# 1.1 получение открытого ключа сервера
 e = byteslib.from_bytes(netlib.receive_bytes(sock))
 n = byteslib.from_bytes(netlib.receive_bytes(sock))
 
@@ -15,11 +18,12 @@ KEY = (e, n)
 
 print("Server key is", KEY)
 
+# 1.2 Отправка своего открытого ключа
 self_e, self_n = PUBLIC_KEY
 netlib.send_bytes(sock, byteslib.to_bytes(self_e))
 netlib.send_bytes(sock, byteslib.to_bytes(self_n))
 
-
+# 1. Регистрация ника (необходимо только при первом подключении)
 print("Auth...")
 
 cmd = {
@@ -32,6 +36,7 @@ print("Waiting for answer...")
 answer = netlib.receive_and_decrypt(sock, PRIVATE_KEY)
 print("Got cmd: ", answer.decode('utf-8'))
 
+# 2. Получение списка пользователей с сервера
 cmd = {
     "cmd": "get_users"
 }
@@ -41,7 +46,7 @@ print("Waiting for answer...")
 answer = netlib.receive_and_decrypt(sock, PRIVATE_KEY)
 print("Got cmd: ", answer.decode('utf-8'))
 
-# message
+# 3. Отправка сообщения
 cmd = {
     "cmd": "send_message",
     "user": "roctbb",
@@ -53,8 +58,7 @@ print("Waiting for answer...")
 answer = netlib.receive_and_decrypt(sock, PRIVATE_KEY)
 print("Got cmd: ", answer.decode('utf-8'))
 
-# get messages
-# message
+# 4. Получение списка сообщений
 cmd = {
     "cmd": "get_messages",
     "user": "roctbb",
@@ -65,7 +69,7 @@ print("Waiting for answer...")
 answer = netlib.receive_and_decrypt(sock, PRIVATE_KEY)
 print("Got cmd: ", answer.decode('utf-8'))
 
-# log out
+# 5. Отключение
 cmd = {
     "cmd": "goodbye"
 }
